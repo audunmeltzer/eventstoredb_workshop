49656dae71815f676a69ee32166592ebd21d0df7
Audun Meltzer <audun.meltzer@gmail.com>
2023-03-28 10:24:19 +0200

    Create task_4 branch

--- api/src/main/kotlin/org/demo/eventstoredb/eventstore/AccountProjection.kt
+++ api/src/main/kotlin/org/demo/eventstoredb/eventstore/AccountProjection.kt
@@ -3,9 +3,6 @@ package org.demo.eventstoredb.eventstore
 import com.eventstore.dbclient.ResolvedEvent
 import com.eventstore.dbclient.Subscription
 import com.eventstore.dbclient.SubscriptionListener
-import org.demo.eventstoredb.eventstore.events.AccountCreated
-import org.demo.eventstoredb.eventstore.events.Deposit
-import org.demo.eventstoredb.eventstore.events.Withdrawal
 import org.demo.eventstoredb.api.model.Account
 
 const val BY_CATEGORY_STREAM_NAME = "\$ce-$STREAM_NAME"
@@ -18,18 +15,7 @@ class AccountProjection : SubscriptionListener() {
     override fun onEvent(subscription: Subscription, resolvedEvent: ResolvedEvent) {
         val aggregateId = extractAccountIdFromResolvedEvent(resolvedEvent)
         val event = resolvedEvent.event.toEvent()
-        println("Handle event ${resolvedEvent.event.eventId} from projection on aggregate $aggregateId")
-        when(event){
-            is AccountCreated -> accounts[aggregateId] = AccountAggregate().handle(event).build()
-            is Deposit -> {
-                accounts[aggregateId]?.let { account ->
-                    accounts.put(aggregateId, account.copy(balance = account.balance + event.amount))
-                }
-            }
-            is Withdrawal -> accounts[aggregateId]?.let { account ->
-                accounts.put(aggregateId, account.copy(balance = account.balance - event.amount))
-            }
-        }
+        //TODO update accounts with new account balance
     }
 
     private fun extractAccountIdFromResolvedEvent(resolvedEvent: ResolvedEvent) =
